163d97da93ef2df0fea0090cbc6c4357eabb3720
henge <50880279+henghenggege@users.noreply.github.com>
2022-10-11 14:49:24 -0400

    结束一周的效果

---
+++ Assets/scripts/EndControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject jieshuMenu;
+
+    public void End()
+    {
+        jieshuMenu.SetActive(true);
+    }
+}
---
+++ Assets/scripts/Rsik.cs
@@ -0,0 +1,22 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rsik : MonoBehaviour
+{
+
+    pubilc float risk = 50f;
+
+    private Image Risk_image;
+    
+    Risk_image=GameObject.Find("Risk Level").GetComponent<Image>();
+
+
+    // Start is called before the first frame update
+    pubilc void takerisk(float amount)
+    {
+        risk += amount;
+        Risk_image.fillAmount = risk / 100f;
+    }
+}*/
